89a9a9e4a81f65a1b2a9a754b902a6413ea69668
tomaselmgren <64467731+tomaselmgren@users.noreply.github.com>
2023-05-10 12:59:21 +0200

    added function

---
+++ prototype/target/debug/deps/fileconverter-24a2d8792fb881ec.d
@@ -0,0 +1,5 @@
+/Users/tomaselmgren/Skola/olleth-tmelm-project/prototype/target/debug/deps/fileconverter-24a2d8792fb881ec: src/main.rs
+
+/Users/tomaselmgren/Skola/olleth-tmelm-project/prototype/target/debug/deps/fileconverter-24a2d8792fb881ec.d: src/main.rs
+
+src/main.rs:
---
+++ prototype/target/debug/fileconverter.d
@@ -0,0 +1,1 @@
+/Users/tomaselmgren/Skola/olleth-tmelm-project/prototype/target/debug/fileconverter: /Users/tomaselmgren/Skola/olleth-tmelm-project/prototype/src/main.rs
